3d70087740d338e83db5f07bcef39f00fe937520
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-19 13:20:19 +0100

    Bug fixed

--- Pig/Program.cs
+++ Pig/Program.cs
@@ -7,12 +7,13 @@ namespace Pig
         static void Main(string[] args)
         {
             Console.WriteLine("Pig dice game");
-          //  bool option = true;
-           // int choice = 0;
-           //=======================KOMENTE PER RREGULLIM ERRORI==================
-           //KUR DEL NJE PIKET TOTALE FSHIHEN TE GJITHA!
-           //RREGULLOJE QE TE RRUAJ PIKET TOTALE TE FITUARA DHE TE FSHIHEN ATO QE NUK KA BERE SAVE
-           //======================================================================
+            //  bool option = true;
+            // int choice = 0;
+            //=======================KOMENTE PER RREGULLIM ERRORI==================
+            //KUR DEL NJE PIKET TOTALE FSHIHEN TE GJITHA!
+            //RREGULLOJE QE TE RRUAJ PIKET TOTALE TE FITUARA DHE TE FSHIHEN ATO QE NUK KA BERE SAVE
+            //======================================================================
+           // int saveScore = 0; 
             int score1 = 0;
             int score2 = 0;
             int temp = 0;
@@ -25,9 +26,10 @@ namespace Pig
             // if(option == true)
             do
             {
-                Console.WriteLine("================================================");
-                b = score2 + temp1;
-                Console.WriteLine("USER2 TOTAL POINTS ARE: " + b);
+                 
+                Console.WriteLine("=============/////USER1 TURN/////========================");
+               // b = score2 + temp1;
+                Console.WriteLine("USER2 TOTAL POINTS ARE: " + score2);
                 Console.WriteLine();
                 Console.WriteLine("User1 turn roll the dice");
                 Console.WriteLine();
@@ -36,7 +38,7 @@ namespace Pig
                 Console.WriteLine("You rolled the dice and got: " + rand_num);
                 Console.WriteLine();
                 temp += rand_num;
-                if (rand_num == 1)
+                if (rand_num == 1) //KJO PUNON
                 {
                     temp = 0;
                     Console.WriteLine("You lost your earned scores and now you have: " +  temp);
@@ -45,7 +47,8 @@ namespace Pig
                 }
                 else
                 {
-                    Console.WriteLine("Total points earned are: " + temp);
+                    b = score1 + temp;
+                    Console.WriteLine("Total points earned are: " + score1 + " and if you save your total score is " + b);
                     Console.WriteLine();
                     Console.WriteLine("Do you want to roll again or hold press y for yes and n for no");
                     Console.WriteLine();
@@ -53,11 +56,14 @@ namespace Pig
                 }
                 if(c == "n")
                 {
+                    
                     do
                     {
-                        Console.WriteLine("=========================================");
-                        a = score1 + temp;
-                        Console.WriteLine("USER1 TOTAL POINTS ARE: " + a);
+                        Console.WriteLine("===========////USER2 TURN/////=====================");
+                        score1 += temp;
+                        temp = 0;
+                        //a = score1 + temp;
+                        Console.WriteLine("USER1 TOTAL POINTS ARE: " + score1);
                         Console.WriteLine();
                         Console.WriteLine("User2 turn roll the dice");
                         Console.WriteLine();
@@ -74,13 +80,14 @@ namespace Pig
                         if (rand_num1 == 1)
                         {
                             temp1 = 0;
-                            Console.WriteLine("You lost your earned scores and now you have: " + temp1);
+                            Console.WriteLine("You lost your earned scores and now you have: " + score2);
                             c = "y";
                             Console.WriteLine();
                         }
                         else
                         {
-                            Console.WriteLine("Total points earned are: " + temp1);
+                            a = score2 + temp1;
+                            Console.WriteLine("Total points earned are: " + score2 + " and if you save your total score is " + a);
                             Console.WriteLine();
                             Console.WriteLine("Do you want to roll again or hold press y for yes and n for no");
                             Console.WriteLine();
@@ -88,8 +95,9 @@ namespace Pig
                         }
                         if (c == "y")
                         {
-                            b = score2 + temp1;
-                            Console.WriteLine("Total score for User1 is:  " + b);
+                            score2 += temp1;
+                            //b = score2 + temp1;
+                            //Console.WriteLine("Total score for User1 is:  " + score2);
                             Console.WriteLine();
                         }
                     } while (c == "n");
